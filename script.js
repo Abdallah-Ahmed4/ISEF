37fae525ba54972a71c939a2b46e4dc12ba4d29a
Abdallah-Ahmed4 <143320878+Abdallah-Ahmed4@users.noreply.github.com>
2023-10-25 12:07:02 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,33 @@
+const video = document.getElementById('video');
+const startButton = document.getElementById('startButton');
+const switchCameraButton = document.getElementById('switchCameraButton');
+let stream;
+let currentCamera = 'user'; // الكاميرا الأمامية
+
+startButton.addEventListener('click', async () => {
+    try {
+        stream = await navigator.mediaDevices.getUserMedia({ video: { facingMode: currentCamera } });
+        video.srcObject = stream;
+    } catch (error) {
+        console.error('Error accessing camera:', error);
+    }
+});
+
+switchCameraButton.addEventListener('click', () => {
+    if (stream) {
+        const tracks = stream.getTracks();
+        tracks.forEach(track => track.stop());
+        video.srcObject = null;
+    }
+    if (currentCamera === 'user') {
+        currentCamera = 'environment'; // الكاميرا الخلفية
+    } else {
+        currentCamera = 'user'; // الكاميرا الأمامية
+    }
+    startButton.click();
+});
+
+
 // استخراج الصورة من الفيديو
 var image = document.querySelector("#video").capture();
 
@@ -7,4 +37,10 @@ var objects = tf.image.objectDetection(image, coco.model);
 // عرض أسماء الأشياء
 for (var object of objects) {
   console.log(object.name);
+}
+// عرض أسماء الأشياء في موقع الويب
+for (var object of objects) {
+  var text = document.createElement("p");
+  text.textContent = object.name;
+  document.body.appendChild(text);
 }
