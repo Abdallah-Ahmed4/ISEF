c05bf9fad3971d8d24624be8e048a174e18d16f6
Abdallah-Ahmed4 <143320878+Abdallah-Ahmed4@users.noreply.github.com>
2023-12-16 18:57:30 +0200

    Delete js directory

--- js/model.js
+++
@@ -1,8 +0,0 @@
-// استيراد مكتبة TensorFlow.js
-import * as tf from "@tensorflow/tfjs";
-
-// تصدير دالة لتحميل نموذج COCO SSD
-export async function loadCocoModel() {
-  const model = await tf.loadGraphModel("https://cdn.jsdelivr.net/npm/@tensorflow-models/coco-ssd");
-  return model;
-}
